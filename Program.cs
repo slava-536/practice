60e3e5457db3009fe0450434663e9defb4405863
Vyacheslav <slavochka-gladkov@mail.ru>
2024-02-01 21:18:46 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+List<byte> nums = new List<byte>() {
+    15, 22, 100, 10, 5
+};
+nums.Add(30);
+nums.Add(20);
+nums.Add(1);
+nums.Reverse();
+nums.Remove(100);
+nums.Sort();
+foreach (byte el in nums)
+{
+    Console.WriteLine("El: " + el);
+}
